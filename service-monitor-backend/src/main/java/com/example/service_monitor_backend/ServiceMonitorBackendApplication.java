12aba6a6411c2d203f031d015aae24595f92e49d
weizhen94 <weizhen94@gmail.com>
2025-11-10 21:20:37 +0800

    10/11/2025 2120

---
+++ service-monitor-backend/src/main/java/com/example/service_monitor_backend/ServiceMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_monitor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMonitorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMonitorBackendApplication.class, args);
+	}
+
+}
---
+++ service-monitor-backend/src/test/java/com/example/service_monitor_backend/ServiceMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service_monitor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMonitorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
